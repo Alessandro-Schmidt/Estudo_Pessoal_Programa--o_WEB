107bfad6a85ab5f995256e9554d452b7c70458ab
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-26 02:29:16 -0300

    Update aula1.js

---
+++ Aulas_Universidade/aulas_JavaScript/aula1.js
@@ -0,0 +1,13 @@
+/*
+Como é um sistema web completo? 
+Ele é formado por:
+Js; (Cliente)
+CSS; (Cliente)
+PHP; (Servidor)
+SQL; (Servidor)
+HTML; (Cliente)
+JavaScript sim, é linguagem de programação. 
+Ele é a parte lógica de um frontend. 
+JavaScript é uma linguagem de programação interpretada; 
+
+*/
