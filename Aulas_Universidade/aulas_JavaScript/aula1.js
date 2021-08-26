19a49f63367b93dbb7b5a69f513e84d162539b11
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-26 13:46:57 -0300

    alterações

--- Aulas_Universidade/aulas_JavaScript/aula1.js
+++ Aulas_Universidade/aulas_JavaScript/aula1.js
@@ -13,8 +13,17 @@ Esse código consegue interagir com o usuário sem a necessidade de interação 
 ELe não grava dados. 
 Ela resolve problemas de front-end
 */
+/*
 window.onload = function(){
     var valor = document.getElementById("nome").value;
     alert(valor+" foi digitado");
+    // Em java, existe a biblioteca jQuery, que ajuda muito os códigos em js. 
+}
+*/
+function somar() {
+    var valor1 = document.getElementById("valor1").value;
+    var valor2 = document.getElementById("valor2").value;
+    var soma = valor1+valor2;
+    alert("A soma de "+valor1+" e "+valor2+" resulta em "+soma); 
 }
 
