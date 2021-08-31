d8e1a5fc434f2c2af77caa0a80c3f796d831c527
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-31 01:07:08 -0300

    Update aula1.js

--- aulas_HTML_cursoemvídeo/curso_em_video_JS/Módulo A/aula1.js
+++ aulas_HTML_cursoemvídeo/curso_em_video_JS/Módulo A/aula1.js
@@ -2,7 +2,8 @@
 O que faz o JavaScript? 
 Linguagem client side. 
 um website é composto por três tecnologias: 
-HTML -informação 
-CSS - Estilo 
-JavaScript - Programação
+HTML -informação;  
+CSS - Estilo; 
+JavaScript - Programação; 
+
 */
