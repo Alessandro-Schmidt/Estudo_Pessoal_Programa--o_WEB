1a04ffbff287c5743c725918ceb9c6943916cff2
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-31 01:05:48 -0300

    Update aula1.js

--- aulas_HTML_cursoemvídeo/curso_em_video_JS/Módulo A/aula1.js
+++ aulas_HTML_cursoemvídeo/curso_em_video_JS/Módulo A/aula1.js
@@ -1,3 +1,8 @@
 /*Conhecendo o JavcaScript
 O que faz o JavaScript? 
+Linguagem client side. 
+um website é composto por três tecnologias: 
+HTML -informação 
+CSS - Estilo 
+JavaScript - Programação
 */
