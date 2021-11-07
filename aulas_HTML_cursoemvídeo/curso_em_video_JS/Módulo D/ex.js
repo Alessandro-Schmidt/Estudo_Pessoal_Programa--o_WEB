45c5232e80226be599346fc5d1c77234977a2db9
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-11-07 19:32:52 -0300

    funcionou uhuuuu

---
+++ aulas_HTML_cursoemvídeo/curso_em_video_JS/Módulo D/ex.js
@@ -0,0 +1,17 @@
+//console.log('Olá mundo.')
+/*var vel = 150
+var lim = 60
+console.log(`Sua velocidade é de ${vel} km/h`)
+if (vel>lim){
+    console.log('Você ultrapassou o limite de velocidade. Multado')
+}else{
+    console.log('Segue teu rumo campeão.')
+}
+
+var pais = 'Suíça'
+console.log(`Vivendo em ${pais},`)
+if (pais == 'Brazil'){
+    console.log('você é Brasileiro.')
+}else{
+    console.log('você é Estrangeiro.')
+}*/
